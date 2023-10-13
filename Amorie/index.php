507f367aa946f7c09566a29ad63d56150bdf0e24
Morris Jansen <morris8jansen@hotmail.com>
2023-10-13 17:00:03 +0200

    de zoekbalk beter gestijld

--- Amorie/index.php
+++ Amorie/index.php
@@ -25,30 +25,16 @@
   <nav class="navbar bg-body-tertiary navbar-expand-lg sticky-top">
     <div class="container-fluid">
 
-<!-- dit is een probeersel kan ik eventueel gewoon verwijderen -->
 
-<div class="mobile-navbar">
-    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand d-flex justify-content-center" href="#">
-      <img class="navbar-logo" src="./images/amorie-logo.png" alt="logo">
-    </a>
-    <!-- Voeg hier de iconen voor mobiel in -->
-  </div>
+<!-- zoekbalk voor grote schermen -->
 
-
-
-
-<!-- zoekbalk -->
-
-<div class="container-zoekbalk">
+<div class="container-zoekbalk grote-zoeker">
   <div class="row">
-    <div class="col-md-6 col-12">
+    <div class="col-11">
       <form class="d-flex">
         <div class="input-group">
           <input class="form-control" type="search" placeholder="Zoek..." aria-label="Zoeken">
-          <button class="btn" type="submit">
+          <button class="btn grote-zoek-knop" type="submit">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
@@ -61,6 +47,8 @@
 
 
 
+
+
 <!-- eerste navbar en hamburger -->
     
       <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas"
@@ -70,59 +58,62 @@
       <a class="navbar-brand d-flex justify-content-center" href="#">
         <img class="navbar-logo" src="./images/amorie-logo.png" alt="logo">
       </a>
-      <div class="offcanvas offcanvas-start d-flex" tabindex="-1" id="offcanvasNavbar"
-        aria-labelledby="offcanvasNavbarLabel">
-        <div class="offcanvas-header">
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+
+      <!-- hamburger toggler-->
+
+        <div class="offcanvas offcanvas-start d-flex" tabindex="-1" id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+
+          <!-- binnenkant hamburger -->
+
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 d-lg-none">
+              <!-- alle links -->
+              <a class="nav-item" href="index.html">
+                <h5 class="my-2 h4 nav-link active" aria-current="page" style="color: #0047AB;">Home</h5>
+              </a>
+
+
+              <a class="nav-item" href="new.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Nieuw</h5>
+              </a>
+
+
+
+              <a class="nav-item" href="sieraden.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Sieraden</h5>
+              </a>          
+
+
+
+
+
+              <a class="nav-item" href="accessoires.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Accesoires</h5>
+              </a>
+
+
+              <a class="nav-item" href="cadeauwinkel.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Cadeauwinkel</h5>
+              </a>
+
+
+              <a class="nav-item" href="collecties.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Collecties</h5>
+              </a>
+
+              
+              <a class="nav-item" href="persoonlijk.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Persoonlijk</h5>
+              </a>
+
+            </ul>
+          </div>
         </div>
-
-
-        <!-- binnenkant hamburger -->
-
-        <div class="offcanvas-body">
-          <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 d-lg-none">
-            <!-- alle links -->
-            <a class="nav-item" href="index.html">
-              <h5 class="my-2 h4 nav-link active" aria-current="page" style="color: #0047AB;">Home</h5>
-            </a>
-
-
-            <a class="nav-item" href="new.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Nieuw</h5>
-            </a>
-
-
-
-            <a class="nav-item" href="sieraden.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Sieraden</h5>
-            </a>          
-
-
-
-
-
-            <a class="nav-item" href="accessoires.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Accesoires</h5>
-            </a>
-
-
-            <a class="nav-item" href="cadeauwinkel.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Cadeauwinkel</h5>
-            </a>
-
-
-            <a class="nav-item" href="collecties.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Collecties</h5>
-            </a>
-
-            
-            <a class="nav-item" href="persoonlijk.html">
-              <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Persoonlijk</h5>
-            </a>
-
-          </ul>
-        </div>
-      </div>
     
 
     <!-- aan de rechterkant groot scherm logo links -->
@@ -151,7 +142,7 @@
 
 
     <!-- klein scherm logo links rechtsboven op het scherm -->
-    <div class="d-flex align-items-center ms-auto d-lg-none">
+    <div class="d-flex align-items-center ms-auto d-lg-none sm-icons-container">
       <a href="" class="sm-navbar-icons">
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-suit-heart" viewBox="0 0 16 16">
   <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
@@ -173,6 +164,30 @@
 
     </div>
   </nav>
+
+
+
+  <!-- zoekbalk voor kleine schermen -->
+  <div class="container-zoekbalk d-lg-none kleine-zoeker">
+  <div class="row">
+    <div class="col-11">
+      <form class="d-flex">
+        <div class="input-group">
+          <input class="form-control" type="search" placeholder="Zoek..." aria-label="Zoeken">
+          <button class="btn kleine-zoek-knop" type="submit">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+            </svg>
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+
+
+
 
 
 
@@ -201,7 +216,7 @@
               <a class="nav-link" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="new.html">Nieuw</a>
+              <a class="nav-link" href="new.php">Nieuw</a>
             </li>
 
 
@@ -249,12 +264,12 @@
 <!-- alert -->
 
 
-
-  <!-- <div class="alert alert-primary custom-alert alert-dismissible fade show text-white text-center" role="alert">
+<div class="alert-container">
+  <div class="alert alert-primary custom-alert alert-dismissible fade show text-white text-center" role="alert">
     gratis verzending vanaf €15
     <button type="button" class="btn-close alert-button" data-bs-dismiss="alert" aria-label="Close"></button>
-  </div> -->
-
+  </div>
+  </div>
 
 
 <!-- eerste foto -->
---
+++ Amorie/new.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nieuw</title>
+</head>
+<body>
+  
+
+
+
+<div class="mobile-navbar">
+    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand d-flex justify-content-center" href="#">
+      <img class="navbar-logo" src="./images/amorie-logo.png" alt="logo">
+    </a>
+
+    <div class="d-none d-lg-flex align-items-center">
+      <a href="" class="logo-link">
+  
+
+
+
+  <div class="d-flex align-items-center ms-auto d-lg-none">
+      <a href="" class="sm-navbar-icons">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-suit-heart" viewBox="0 0 16 16">
+  <path d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"/>
+</svg>
+      </a>
+
+      <a href="" class="sm-navbar-icons">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-bag" viewBox="0 0 16 16">
+  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z"/>
+</svg>
+      </a>
+
+      <a href="" class="sm-navbar-icons">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg>
+      </a>
+
+    </div>
+
+
+
+
+
+
+  
+
+
+  <div class="offcanvas offcanvas-start d-flex" tabindex="-1" id="offcanvasNavbar"
+          aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+
+
+          <!-- binnenkant hamburger -->
+
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 d-lg-none">
+              <!-- alle links -->
+              <a class="nav-item" href="index.html">
+                <h5 class="my-2 h4 nav-link active" aria-current="page" style="color: #0047AB;">Home</h5>
+              </a>
+
+
+              <a class="nav-item" href="new.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Nieuw</h5>
+              </a>
+
+
+
+              <a class="nav-item" href="sieraden.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Sieraden</h5>
+              </a>          
+
+
+
+
+
+              <a class="nav-item" href="accessoires.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Accesoires</h5>
+              </a>
+
+
+              <a class="nav-item" href="cadeauwinkel.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Cadeauwinkel</h5>
+              </a>
+
+
+              <a class="nav-item" href="collecties.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Collecties</h5>
+              </a>
+
+              
+              <a class="nav-item" href="persoonlijk.html">
+                <h5 class="my-2 h4 nav-link" style="color: #0047AB;">Persoonlijk</h5>
+              </a>
+
+            </ul>
+          </div>
+        </div>
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="script.js"></script>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+
+</body>
+</html>
